2aceeccb3dc14e6e91d97ac3012586162d815002
ilkosta <costantino.giuliodori@gmail.com>
2014-05-16 11:42:02 +0200

    messages

--- pull_repos.fish
+++ pull_repos.fish
@@ -1,14 +1,17 @@
-function update_repos
+function pull_repos
 	set -l start_dir $PWD
 
   for p in (find . -type d -name .git)
   	cd $p
   	cd ..
+    echo '---------------------------------------------'
+    echo "- checking $PWD"
   	git remote show origin | grep http >/dev/null
-  		and echo '---------------------------------------------'
-  		and echo "update $PWD"
-  		and echo '---------------------------------------------'
+  		
+  		and echo " *** update $PWD"
+  		
   		and git pull
   	cd $start_dir
+    and echo '---------------------------------------------'
   end
 end
